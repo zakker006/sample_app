8092fe8ebd88843b8ab4680c4e9e17cdf76847d1
EC2 Default User <ec2-user@ip-172-31-14-62.ec2.internal>
2021-10-30 22:28:42 +0000

    [end]Railsを学ぼう

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,45 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0b436fc44c929b0f9493af6f1ab7e9505d2a0919baf5fad107bf36af8cdfe49c5cbcba12df659d27f0fed555ed05d5ab30096adeff9c78c1f3b5928b88187e54'
